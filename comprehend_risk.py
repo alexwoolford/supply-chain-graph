a546998a9a973165b62991a84af104e9b8fd49f6
Alex Woolford <alex@woolford.io>
2021-10-11 22:41:30 -0600

    risks written to graph

--- comprehend_risk.py
+++ comprehend_risk.py
@@ -2,7 +2,9 @@ import boto3
 import glob
 from pathlib import Path
 from funcy import project
-
+import pandas as pd
+from neo4j import GraphDatabase
+import os
 
 def get_entities_from_text(text):
     comprehend = boto3.client(service_name='comprehend', region_name='us-west-2')
@@ -21,6 +23,7 @@ def get_chunks(text, maxlength):
 
 risk_text_files = glob.glob("data/10k_risk_text/*")
 
+all_entities = list()
 for risk_text_file in risk_text_files:
     fh = open(risk_text_file, 'r')
     text = fh.read()
@@ -29,7 +32,6 @@ for risk_text_file in risk_text_files:
 
     ticker = Path(risk_text_file).stem
 
-    all_entities = list()
     for chunk in chunks:
         entities = get_entities_from_text(text=chunk).get('Entities')
         entities = [dict(entity, **{'Ticker': ticker}) for entity in entities]
@@ -39,5 +41,36 @@ for risk_text_file in risk_text_files:
 
         all_entities += entities
 
-    pass
+all_entities_df = pd.DataFrame(all_entities)
+
+all_entities_df['Type'] = all_entities_df['Type'].apply(lambda x: x.capitalize())
+
+entity_types_to_keep = ['Location', 'Organization', 'Other']
+all_entities_df = all_entities_df.query("Type in @entity_types_to_keep")
+
+all_entities_df = all_entities_df.rename(columns={'Type': 'label', 'Text': 'text', 'Ticker': 'ticker'})
+
+
+neo4j_uri = os.getenv("NEO4J_URI")
+neo4j_user = os.getenv("NEO4J_USER")
+neo4j_password = os.getenv("NEO4J_PASSWORD")
+
+# Neo4j DB connection
+driver = GraphDatabase.driver(neo4j_uri, auth=(neo4j_user, neo4j_password))
+
+for index, row in all_entities_df.iterrows():
+    label = str(row["label"])
+    text = str(row["text"])
+    ticker = str(row["ticker"])
+    cypher = """MATCH(t:Ticker)
+                WHERE t.ticker = $ticker 
+                MERGE(x:{label} {{text: $text}})
+                MERGE(t)-[:HAS_RISK]->(x) 
+                """.format(label=label)
+    with driver.session(database="supplychain") as session:
+        session.run(cypher,
+                    label=label,
+                    text=text,
+                    ticker=ticker)
+
 
